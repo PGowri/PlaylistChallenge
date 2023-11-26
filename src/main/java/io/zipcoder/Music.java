30eacde4d11c64cd331540116e458d0ec393eaba
Gowri <gowri@ZipcodersMBP64.local>
2023-11-26 17:23:43 -0500

    one test working

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -5,10 +5,25 @@ public class Music {
     private String[] playList;
 
     public Music(String[] playList){
+
         this.playList = playList;
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        int counter = 0;
+        for(int i =0; i< playList.length; i++) {
+            if (selection.equals(playList[i])) {
+                counter = i - startIndex;
+//                break;
+            }
+              else if (startIndex < 0){
+                startIndex = playList.length -1;
+            } else if (startIndex >= playList.length) {
+                startIndex = 0 ;
+            }
+        }
+
+
+        return counter;
     }
 }
